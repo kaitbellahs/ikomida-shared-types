8b69edd7f6c00d1bb4bfb5fd16985d5f7ea6208f
khalid ait bellahs <kaitbellahs@gmail.com>
2022-08-04 02:34:59 -0300

    Update

--- src/DiscountTypes.mjs
+++ src/DiscountTypes.mjs
@@ -1,7 +1,7 @@
 export default class DiscountTypes {
-    static PERCENT = 'percent';
-    static VALUE = 'value';
-    static NO = 'no discount';
+    static PERCENT = 'PERCENT';
+    static VALUE = 'VALUE';
+    static NO = 'NO';
 
     static keys = Object.keys(DiscountTypes)
     static list = DiscountTypes.keys.map((discountType) => {

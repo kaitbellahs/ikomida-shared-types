a808846f2fc4cf99ba9129cc6f7f79a422d493db
khalid ait bellahs <kaitbellahs@gmail.com>
2022-08-17 17:53:05 -0300

    Update

--- src/DiscountTypes.mjs
+++ src/DiscountTypes.mjs
@@ -1,13 +1,37 @@
 export default class DiscountTypes {
+    description
+    name
+    id
+    constructor(type) {
+        this.id = type
+        switch (type) {
+            case DiscountTypes.PERCENT:
+                this.name = 'porcentagem'
+                this.description = 'descontar um porcentagem'
+                break
+            case DiscountTypes.VALUE:
+                this.name = 'valor'
+                this.description = 'descontar um valor'
+                break
+            case DiscountTypes.NO:
+                this.name = 'sem descontar';
+                this.description = 'sem nenhum desconto';
+                break
+            default:
+                this.name = '-'
+                this.description = '-'
+                break
+        }
+    }
     static PERCENT = 'PERCENT';
     static VALUE = 'VALUE';
     static NO = 'NO';
 
     static keys = Object.keys(DiscountTypes)
-    static list = DiscountTypes.keys.map((discountType) => {
+    static list = DiscountTypes.keys.map((item) => {
         return {
-            id: discountType,
-            name: DiscountTypes[discountType]
+            id: item,
+            name: DiscountTypes(item)?.name
         };
     })
 }

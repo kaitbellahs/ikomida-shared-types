98cbc099b61d14477cb4f46fb09d26bc83dd5c6a
khalid ait bellahs <kaitbellahs@gmail.com>
2022-08-26 18:27:35 -0300

    Update

---
+++ src/UserBankAccount.mjs
@@ -0,0 +1,45 @@
+import BaseType from './BaseType.mjs'
+export default class UserBankAccount extends BaseType {
+
+    constructor(type) {
+        super(type)
+        switch (type) {
+            case UserBankAccount.CNPJ:
+                this.name = 'CNPJ'
+                this.description = 'CNPJ'
+                break
+            case UserBankAccount.CPF:
+                this.name = 'CPF'
+                this.description = 'CPF'
+                break
+            case UserBankAccount.PHONE:
+                this.name = 'telefone';
+                this.description = 'telefone';
+                break
+            case UserBankAccount.EMAIL:
+                this.name = 'email';
+                this.description = 'email';
+                break
+            case UserBankAccount.RANDOM:
+                this.name = 'aleatoria';
+                this.description = 'aleatoria';
+                break
+            case UserBankAccount.BANK_ACCOUNT:
+                this.name = 'conta bancaria';
+                this.description = 'conta bancaria';
+                break
+            default:
+                this.name = '-'
+                this.description = '-'
+                break
+        }
+    }
+
+    static CPF = 'CPF';
+    static CNPJ = 'CNPJ';
+    static PHONE = 'PHONE';
+    static EMAIL = 'EMAIL';
+    static RANDOM = 'RANDOM';
+    static BANK_ACCOUNT = 'BANK_ACCOUNT';
+
+}

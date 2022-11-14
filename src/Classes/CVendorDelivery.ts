9ecccc4f07d70cd18139ede8a7ea4fbfef3463a9
khalid ait bellahs <khalid@tialtonivel.com.br>
2022-11-14 12:39:48 -0300

    Update user role type, add vendor deliveryorder min value

--- src/Classes/CVendorDelivery.ts
+++ src/Classes/CVendorDelivery.ts
@@ -7,9 +7,11 @@ export default class CVendorDelivery extends BaseJSON {
   @Property
   value!: number
   @Property
+  orderMinValue!: number
+  @Property
   min!: number
 
-  static init(free: boolean, value: number, min: number, id?: string, timestamp?: number): CVendorDelivery {
-    return this.createInitObject(arguments, ['free', 'value', 'min', 'id', 'timestamp'])
+  static init(free: boolean, value: number, orderMinValue: number, min: number, id?: string, timestamp?: number): CVendorDelivery {
+    return this.createInitObject(arguments, ['free', 'value', 'orderMinValue', 'min', 'id', 'timestamp'])
   }
 }

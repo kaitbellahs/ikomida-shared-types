a2498e77474be912c9adf13d36ea3660192fac07
Khalid Ait Bellahs <kaitbellahs@gmail.com>
2023-01-19 16:01:27 -0300

    Add rollup iKomida plugin

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,53 @@ import resolve from "@rollup/plugin-node-resolve";
 import { terser } from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
 import { createRequire } from 'module'
+import beep from '@rollup/plugin-beep'
+import strip from '@rollup/plugin-strip'
 const require = createRequire(import.meta.url)
 const pkg = require('./package.json')
 const tsconfig = require('./tsconfig.json')
+import ast from "abstract-syntax-tree"
+const { parse, generate, remove } = ast;
+const fs = require('fs');
+
+function iKomidaPlugin() {
+    return {
+        name: "iKomida-plugin",
+        renderChunk(code, chunk, options, meta) {
+            if (chunk.type === 'chunk') {
+                const isUMD = ['umd', 'iife'].includes(options.format)
+                const classesArray = []
+                const tree = parse(code)
+                const array = (isUMD ? tree.body[0].expression.arguments[1].body.body : tree.body)
+                const iterable = [...array]
+                for (let i = 0, j = 0; i < iterable.length; i++, j++) {
+                    const object = iterable[i]
+                    if (object?.type === 'ClassDeclaration') {
+                        classesArray.push(object)
+                        array.splice(j, 1)
+                        j--
+                    }
+                }
+                if (isUMD) {
+                    tree.body[0].expression.arguments[1].body.body = [...classesArray, ...array]
+                } else {
+                    remove(tree, 'Literal[value="use strict"]')
+                    tree.body = [...classesArray, ...array]
+                }
+                const result = isUMD ? generate(tree) : `"use strict"
+${generate(tree)}`
+                // fs.writeFile(`./debug/${chunk.fileName}`, result, err => {
+                //     if (err) {
+                //         console.error(err);
+                //     }
+                //     // file written successfully
+                // });
+                return result
+            }
+            return code
+        }
+    }
+}
 
 export default [
     {
@@ -16,29 +60,32 @@ export default [
                 inlineDynamicImports: true,
                 file: pkg.exports.node.require,
                 format: 'cjs',
-                sourcemap: true,
+                sourcemap: false,
             },
             {
                 inlineDynamicImports: true,
                 file: pkg.exports.node.import,
                 format: 'es',
-                sourcemap: true,
+                sourcemap: false,
             },
             {
                 inlineDynamicImports: true,
                 name: pkg.name,
                 file: pkg.exports.default,
                 format: "umd",
-                sourcemap: true,
+                sourcemap: false,
             },
         ],
         plugins: [
+            strip(),
+            beep(),
             autoExternal(),
             resolve({
                 preferBuiltins: true
             }),
             json(),
             tsPlugin(tsconfig),
+            iKomidaPlugin(),
             terser({
                 compress: {
                     ecma: 'ESNext',
